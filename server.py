8549d7c8cf393d988b7a3453b1095184e451c86d
Shivam Bharuka <shivam.bharuka@gmail.com>
2017-04-10 17:28:52 -0500

    debugging

--- server.py
+++ server.py
@@ -21,6 +21,7 @@ class Server:
 		self.storage = {}
 
 		self.ack = asyncio.Event()
+		self.replica_ack = asyncio.Event()
 
 		hostname = socket.gethostname().split('-')
 		self.hostNumber = int(hostname[3].split('.')[0])
@@ -167,7 +168,6 @@ class Server:
 			if msg.type == "ACK" and self.ack.is_set() is False:
 				print("{} {}".format(msg.key, msg.value), flush=True)
 				self.ack.set()
-
 			elif msg.type == "SET":
 				await getattr(self.serverRPC, 'handle_{}'.format(msg.type))(msg, self)
 				if msg.owner == self.hostNumber:
@@ -188,6 +188,8 @@ class Server:
 					msgObj = InputMessage('ACK NOT FOUND')
 				msg = pickle.dumps(msgObj)
 				await self.loop.sock_sendall(client, struct.pack('>I', len(msg)) + msg)
+			elif msg.type == "REPLICA":
+				self.replica_ack.set()
 
 		client.close()
 		del self.connections[addr]
@@ -208,22 +210,47 @@ class ServerRequestHandlers:
 		if messageObj.owner == server.hostNumber:
 			msg = pickle.dumps(messageObj)
 			while True:
-				successor = server.find_successor(messageObj.owner)
-				host = server.hostnames[successor - 1]
+				server.replica_ack.clear()
+				while True:
+					successor = server.find_successor(messageObj.owner)
+					host = server.hostnames[successor - 1]
+					try:
+						await server.loop.sock_sendall(server.connections[socket.gethostbyname(host)], struct.pack('>I', len(msg)) + msg)
+						break
+					except OSError as oe:
+						messageObj.findOwner(server)
+
+				#wait for a response until a timeout and then try again
 				try:
-					await server.loop.sock_sendall(server.connections[socket.gethostbyname(host)], struct.pack('>I', len(msg)) + msg)
-					break
-				except OSError as oe:
-					messageObj.findOwner(server)
+					await asyncio.wait_for(server.replica_ack.wait(), 3.0)
+					#check if the request was successfully completed
+					if server.replica_ack.is_set():
+						break
+				except asyncio.TimeoutError as te:
+					continue
+
+			server.replica_ack.clear()
 
 			while True:
-				predecessor = server.find_predecessor(messageObj.owner)
-				host = server.hostnames[predecessor - 1]
+				server.replica_ack.clear()
+				while True:
+					predecessor = server.find_predecessor(messageObj.owner)
+					host = server.hostnames[predecessor - 1]
+					try:
+						await server.loop.sock_sendall(server.connections[socket.gethostbyname(host)], struct.pack('>I', len(msg)) + msg)
+						break
+					except OSError as oe:
+						messageObj.findOwner(server)
+
 				try:
-					await server.loop.sock_sendall(server.connections[socket.gethostbyname(host)], struct.pack('>I', len(msg)) + msg)
-					break
-				except OSError as oe:
-					messageObj.findOwner(server)
+					await asyncio.wait_for(server.replica_ack.wait(), 3.0)
+					#check if the request was successfully completed
+					if server.replica_ack.is_set():
+						break
+				except asyncio.TimeoutError as te:
+					continue
+
+			server.replica_ack.clear()
 
 	async def handle_GET(self, messageObj, server):
 		#find the key and return
